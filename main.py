9f52d47324fc8fdc6f378de47a697766e23fde3e
Martin George <marty@dingo.sh>
2020-09-20 19:05:47 +0100

    Added quick REST implementation

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
 import scrollphathdcustom
 import scrollphathd
 
+# Let's not blind anyone here, unless it's intentionally done using the below line of code. 
 scrollphathd.set_brightness(0.2)
-scrollphathdcustom.scrollMsg("Test")
+
+app = Flask(__name__)
+api = Api(app)
+
+class Notifiy(Resource)
+  def post(Resource):
+      scrollphathdcustom.scrollMsg(apiNotifyMsg)
+
+api.add_resource(Notify, '/notify/<apiNotifyMsg>')
+
+if __name__ == '__main__':
+     app.run(port='5002')
